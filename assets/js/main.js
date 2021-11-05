4b63d67108e1840624dcb4a12772e3a3770d6151
Thanh Dinh <mr.thanhdinh1996@gmail.com>
2021-11-06 00:12:16 +0700

    new-cm 06/11

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
 // admin click
-
 let adminElement = document.querySelector('.header__admin');
 let adminSelect = document.querySelector('.admin-popup');
 adminElement.addEventListener('click', function () {
     adminSelect.classList.toggle('is__open');
+});
+
+// aside-menu-left
+let asideMenuItem = document.querySelectorAll('.aside__menu > li');
+asideMenuItem.forEach(item => {
+    item.addEventListener('click', function (e) {
+        item.classList.add('is__open')
+    })
 });
